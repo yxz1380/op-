61d2f3c854ea40b2d2e0ccb6729c2de23375509f
Netflixxp <61532011+Netflixxp@users.noreply.github.com>
2021-03-16 22:11:27 +0800

    更新默认IP和默认主体

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 替换默认IP
+sed -i 's#192.168.1.1#192.168.1.99#g' package/base-files/files/bin/config_generate
+# 替换默认主题
+sed -i 's#luci-theme-bootstrap#luci-theme-argon_new#g' feeds/luci/collections/luci/Makefile
