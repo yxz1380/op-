2e3f79802426d01e88f6cbe25b2fb35cfd0e2e2d
Netflixxp <61532011+Netflixxp@users.noreply.github.com>
2021-05-01 18:03:58 +0800

    默认主题替换

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,4 @@
 sed -i 's#192.168.1.1#192.168.1.99#g' package/base-files/files/bin/config_generate
 # 替换默认主题
 sed -i 's#luci-theme-bootstrap#luci-theme-argon#g' feeds/luci/collections/luci/Makefile
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
