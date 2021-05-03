00cac36cfe76670801d5f1e6c484de680fbd7ac9
yxz1380 <76253848+yxz1380@users.noreply.github.com>
2021-05-03 15:07:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # 替换默认IP
-sed -i 's#192.168.1.1#192.168.1.99#g' package/base-files/files/bin/config_generate
+sed -i 's#192.168.1.1#192.168.50.254#g' package/base-files/files/bin/config_generate
 # 替换默认主题
 sed -i 's#luci-theme-bootstrap#luci-theme-argon#g' feeds/luci/collections/luci/Makefile
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
